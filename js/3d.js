fd170acfc271973e5be46c799c8f1d017bc1b502
Emanuel Luna <memonluna@gmail.com>
2023-03-25 21:45:17 -0300

    engine 3d, localstorage

---
+++ js/3d.js
@@ -0,0 +1,4 @@
+document.getElementById("marco3d").innerHTML='<object id="htmlInsert" type="text/html" data="3diseno.html" ></object>';
+
+document.getElementById("htmlInsert").setAttribute('height', '100%');
+document.getElementById("htmlInsert").setAttribute('width', '100%');
